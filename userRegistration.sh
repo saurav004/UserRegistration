2777ca5bd4a88ebc89ca084053432978e103b10a
saurav004 <logtosaurav@gmail.com>
2020-10-15 00:32:55 +0530

    ADDED:code that takes last name and checks it's validity

--- userRegistration.sh
+++ userRegistration.sh
@@ -1,12 +1,28 @@
 #!/bin/bash -x
 echo "------------------------------------------------------------------------------WELCOME TO USER REGISTRATION CODE-----------------------------------------------------------------------"
+#turning extglob on
+
 shopt -s extglob
-pat="^[A-Z][a-z]{2,}$"
+
+#declaring pattern
+
+pat1="^[A-Z][a-z]{2,}$"
+
+#reading first and last name
 read -p "Enter your First name" Fname
+read -p "Enter your First name" Lname
 
 if [[ $Fname =~ $pat ]]
 then
-	echo "valid"
+	echo "valid First name"
 else
-	echo "invalid"
+	echo "invalid First name"
 fi
+
+if [[ $Lname =~ $pat ]]
+then
+	echo "valid Last name"
+else
+	echo "invalid Last name"
+fi
+
