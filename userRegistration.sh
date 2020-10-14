2961be554b4f9915cf0f4d482ec829f377cec66b
saurav004 <logtosaurav@gmail.com>
2020-10-15 00:22:34 +0530

    Added:code that takes first name and macthes it's validity

--- userRegistration.sh
+++ userRegistration.sh
@@ -1,2 +1,12 @@
 #!/bin/bash -x
 echo "------------------------------------------------------------------------------WELCOME TO USER REGISTRATION CODE-----------------------------------------------------------------------"
+shopt -s extglob
+pat="^[A-Z][a-z]{2,}$"
+read -p "Enter your First name" Fname
+
+if [[ $Fname =~ $pat ]]
+then
+	echo "valid"
+else
+	echo "invalid"
+fi
