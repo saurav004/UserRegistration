07687f3447ef3082097c7961859b70a3f5c80a4f
saurav004 <logtosaurav@gmail.com>
2020-10-15 20:39:54 +0530

    Added:code to check validity of password as per rule 2

--- userRegistration.sh
+++ userRegistration.sh
@@ -10,11 +10,12 @@ pat1="^[A-Z][a-z]{2,}$"
 pat2="^[a-zA-Z][a-zA-Z0-9_\-+]*[.]{0,1}[a-zA-Z0-9_\-+]{1,}[@][a-zA-Z0-9]{1,}[.][a-zA-Z]{2,}[.]{0,}[a-zA-Z]*$"
 patMobileNumber="^[+]{1}[0-9]{2}[ ][0-9]{10}"
 patPassword1="^[a-zA-Z0-9]{8,}$"
+patPassword2="^(?=.*[A-Z])[a-zA-Z\d]{8,}$"
 read -p "Enter valid password" password
 
-if [[ $password =~ $patPassword1 ]]
+if [[ $password =~ $patPassword2 ]]
 then
-	echo "valid Password according to rule 1"
+	echo "valid Password according to rule 2"
 else
 	echo "invalid"
 fi
